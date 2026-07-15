1cf7fcc1322fed461eec2245ced83e53a53a9d39
pankaj-2702 <pankajkumar67920@gmail.com>
2026-07-15 22:05:56 +0530

    fix bug .. toLower email before storing the user into DATABASE

--- Backend/model/user.js
+++ Backend/model/user.js
@@ -11,6 +11,8 @@ const UserSchema = new mongoose.Schema({
     },
      email : {
         type :String,
+        trim :true,
+        lowercase : true,
         required : [true, 'Please provide email'],
         match : [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       'Please enter a valid email address',
